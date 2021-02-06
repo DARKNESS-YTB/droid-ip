47dd6b764643fbbd04b2194f20afdf4a137adc92
DARKNESS-YTB <78645960+DARKNESS-YTB@users.noreply.github.com>
2021-02-06 22:49:32 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# droid-ip
-hack phone with ip 
+Termux :
+pkg install git
+
+git clone
+
+cd droidip
+
+python droidip.py
+
+Kali linux :
+
+sudo apt install git 
+
+git clone
+
+cd droidip
+
+python droidip.py
+
+DARKNESS YT 
---
+++ droidip.py
@@ -0,0 +1,28 @@
+import os
+os.system('clear')
+print ("HELLO IN MY WORLD")
+darkness = input ("TYPE IP:")
+print ("tybe help please")
+print ("hi")
+DRKNP = input ("DRKN:")
+if DRKNP == "help" :
+ print ("Screen shot")
+ print (" ")
+ print ("send sms")
+ print (" ")
+ print ("password accounts")
+ print (" ")
+ print ("delete data")
+ DRKNP = input ("DRKN :")
+if DRKNP == "Screen shot" :
+  print ("NOT FOUND IP" +darkness )
+  DRKNP = input ("DRKN :")
+if DRKNP == "send sms" :
+  input ("NUMBER :")
+  print ("NOT FOUND IP" +darkness )
+if DRKNP == "password accounts" :
+ v = 0
+ for v in range (5) :
+     print ("NOT FOUND IP" +darkness ) 
+
+
